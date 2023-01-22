9381b5adf091e188328a0403532bdd6d3892653d
shoval <shoval123055@gmail.com>
2023-01-22 13:37:39 +0200

    fix bag tag

--- tag_check.sh
+++ tag_check.sh
@@ -8,10 +8,10 @@ tag_befor=`echo $all_tag |rev| cut -d " " -f2 | rev`
 test_last=`echo $last_tag | cut -d '.' -f1`
 test_befor=`echo $tag_befor | cut -d '.' -f1`
 # if [ "$last_tag" -eq "$tag_befor" ];then
-if [ "$test_last" -gt "$test_befor" ] || [ "$test_last" -eq "$test_befor" ];then
+if [ "$test_last" -eq "$test_befor" ];then
     test_last=`echo $last_tag | cut -d '.' -f2`
     test_befor=`echo $tag_befor | cut -d '.' -f2`
-    if [ "$test_last" -gt "$test_befor" ] || [ "$test_last" -eq "$test_befor" ];then
+    if [ "$test_last" -eq "$test_befor" ];then
     test_last=`echo $last_tag | cut -d '.' -f3`
     test_befor=`echo $tag_befor | cut -d '.' -f3`
         if [ "$test_last" -gt "$test_befor" ];then
@@ -20,10 +20,14 @@ if [ "$test_last" -gt "$test_befor" ] || [ "$test_last" -eq "$test_befor" ];then
         echo "tag not good"
         exit 1
         fi
+    elif [ "$test_last" -gt "$test_befor" ];then
+    echo "$last_tag is good" 
     else
     echo "tag not good"
     exit 1 
     fi
+elif [ "$test_last" -gt "$test_befor" ];then
+echo "$last_tag is good" 
 else
 echo "tag not good"
 exit 1
